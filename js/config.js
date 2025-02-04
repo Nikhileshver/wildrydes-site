dcf6c4a1c221b4a03b7f91b7db52e4326d28e679
Nikhileshver <nikhikeshver@gmail.com>
2025-02-04 10:24:03 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_5oxlCyNqV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2ej3frt0m2qumijqc3l6588ib4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_BDRGbV3nF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4213j8q2499pqbtjlieb9j16rt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
